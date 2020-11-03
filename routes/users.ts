1bb17a052b95b6a0f700ca396b2ce71a9c041b60
praj000 <parajbhattacharjee123@gmail.com>
2020-11-03 11:09:30 +0530

    Hospital Application

--- routes/users.ts
+++ routes/users.ts
@@ -5,5 +5,11 @@ var app = express();
 router.get('/users', function(req, res, next) {
   res.render('Users')
 });
-
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+router.post('/login_post', (req, res) => {
+  console.log(req.body.username);
+  res.redirect(303, '/');
+})
 module.exports = router;
