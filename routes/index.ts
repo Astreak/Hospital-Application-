ebfefbd2915756af6e2f6868915c2f7ec1df6208
praj000 <parajbhattacharjee123@gmail.com>
2020-11-03 21:18:09 +0530

    Hospital application

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,7 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
 
 
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
 
 
-module.exports = router;
+module.exports = app;
--- routes/users.ts
+++ routes/users.ts
@@ -1,22 +1,76 @@
 var express = require('express');
-var router = express.Router();
+const session = require('express-session');
+const cookie = require('cookie-parser');
 var app = express();
-
-router.get('/users', function(req, res, next) {
-  res.render('Users')
+app.use(session({
+  name: 'prj',
+  secret: '*****',
+  saveUnInitialized: false,
+  resave: false,
+  cookie: {
+    secret: false,
+    maxAge: 6000000,
+    path: '/',
+    httpOnly: true,
+    priority: 'Low'
+  }
+}))
+app.use(session({
+  name: 'chess',
+  secret: '*****',
+  saveUnInitialized: false,
+  resave: false,
+  cookie: {
+    secret: false,
+    maxAge: 600000,
+    path: '/',
+    httpOnly: false,
+    priority:'High'
+  }
+}))
+var redirectHome = (req, res, next) => {
+  if (!req.session.prj)
+    next()
+  else
+    res.redirect(303,'/')
+}
+var redirectLogin = (req, res, next) => {
+  if (req.session.prj)
+    next()
+  else
+    res.redirect(303,'/login')
+}
+app.get('/', redirectLogin, function (req, res) {
+   console.log(req.session.cookie.domain)
+   res.render('index');
 });
-router.get('/login', (req, res) => {
+
+app.get('/login',redirectHome, (req, res) => {
   res.render('login')
 })
-router.post('/login_post', (req, res) => {
-  console.log(req.body.username);
-  res.redirect(303, '/');
-})
-router.get('/register', (req, res) => {
-  res.render('register')
-})
-router.post('/register_post', (req, res) => {
-  console.log(req.body.email)
+app.post('/login_post', (req, res) => {
+  req.session.prj = req.body.username
+  req.session.chess = req.body.password
+  console.log(req.session.chess)
+  
+ 
   res.redirect(303,'/')
 })
-module.exports = router;
+app.get('/register', (req, res) => {
+  console.log(req.session.chess)
+  res.render('register')
+})
+app.get('/register/api', (req, res) => {
+  console.log(req.session.chess)
+  res.send("<h1> Stuff </h1>")
+})
+app.post('/register_post', (req, res) => {
+  console.log('Data successfully recorded')
+  res.redirect(303,'/')
+})
+app.get('/logout',redirectLogin, (req, res) => {
+  req.session.destroy()
+  res.clearCookie()
+  res.redirect(303,'/')
+})
+module.exports = app;
