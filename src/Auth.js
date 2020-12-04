66750c1305c5f485bddc48ca9823d4572d28aa71
praj000 <parajbhattacharjee123@gmail.com>
2020-12-04 13:28:19 +0530

    #63

--- src/Auth.js
+++ src/Auth.js
@@ -1,28 +1,4 @@
 var express = require('express')
 app = express()
-var basicAuth = (req, res, next) => {
-   console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
-  console.log('AUTH:',authHeader)
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == process.env.NAME && pass == process.env.past) {
-      next(); 
-  } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
-  }
 
-}
-app.use(basicAuth)
 module.exports=app 
